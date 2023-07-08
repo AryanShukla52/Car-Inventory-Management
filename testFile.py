a13149c06f9211b798b5f55ed157f96938ac1db4
AryanShukla52 <102696882+AryanShukla52@users.noreply.github.com>
2023-07-08 17:10:52 -0600

    Update testFile.py

--- testFile.py
+++ testFile.py
@@ -3,6 +3,88 @@ from Car import Car
 from CarInventoryNode import CarInventoryNode
 from CarInventory import CarInventory
 
+def test_printCar():
+    c = Car("Honda", "CRV", 2007, 8000)
+    assert str(c) == "Make: HONDA, Model: CRV, Year: 2007, Price: $8000"
+
+def test_gtCar():
+    car1 = Car("Mercedes", "CLS", 2018, 35000)
+    car2 = Car("Mercedes", "Sprinter", 2022, 50000)
+    car3 = Car("Mercedes", "Sprinter", 2022, 40000)
+    car4 = Car("Mercedes", "Sprinter", 2014, 25000)
+    car5 = Car("Ford", "Ranger", 2021, 25000)
+    assert (car1 > car5) == True
+    assert (car3 > car1) == True
+    assert (car3 > car4) == True
+    assert (car2 > car3) == True
+    assert (car5 > car1) == False
+
+def test_ltCar():
+    car1 = Car("Mercedes", "CLS", 2018, 35000)
+    car2 = Car("Mercedes", "Sprinter", 2022, 50000)
+    car3 = Car("Mercedes", "Sprinter", 2022, 40000)
+    car4 = Car("Mercedes", "Sprinter", 2014, 25000)
+    car5 = Car("Ford", "Ranger", 2021, 25000)
+    assert (car5 < car1) == True
+    assert (car1 < car3) == True
+    assert (car4 < car3) == True
+    assert (car3 < car2) == True
+    assert (car1 < car5) == False
+
+def test_eqCar():
+    car1 = Car("Mercedes", "CLS", 2018, 35000)
+    car2 = Car("Mercedes", "CLS", 2018, 35000)
+    assert (car1 == car2) == True
+
+def test_BST():
+    bst = CarInventory()
+
+    car1 = Car("Nissan", "Leaf", 2018, 18000)
+    car2 = Car("Tesla", "Model3", 2018, 50000)
+    car3 = Car("Mercedes", "Sprinter", 2022, 40000)
+    car4 = Car("Mercedes", "Sprinter", 2014, 25000)
+    car5 = Car("Ford", "Ranger", 2021, 25000)
+
+    bst.addCar(car1)
+    bst.addCar(car2)
+    bst.addCar(car3)
+    bst.addCar(car4)
+    bst.addCar(car5)
+
+    assert bst.getBestCar("Nissan", "Leaf") == car1
+    assert bst.getBestCar("Mercedes", "Sprinter") == car3
+    assert bst.getBestCar("Honda", "Accord") == None
+
+    assert bst.getWorstCar("Nissan", "Leaf") == car1
+    assert bst.getWorstCar("Mercedes", "Sprinter") == car4
+    assert bst.getBestCar("Honda", "Accord") == None
+
+    assert bst.getTotalInventoryPrice() == 158000
+
+    assert bst.inOrder() == \
+"""\
+Make: FORD, Model: RANGER, Year: 2021, Price: $25000
+Make: MERCEDES, Model: SPRINTER, Year: 2022, Price: $40000
+Make: MERCEDES, Model: SPRINTER, Year: 2014, Price: $25000
+Make: NISSAN, Model: LEAF, Year: 2018, Price: $18000
+Make: TESLA, Model: MODEL3, Year: 2018, Price: $50000
+"""
+    assert bst.preOrder() == \
+"""\
+Make: NISSAN, Model: LEAF, Year: 2018, Price: $18000
+Make: MERCEDES, Model: SPRINTER, Year: 2022, Price: $40000
+Make: MERCEDES, Model: SPRINTER, Year: 2014, Price: $25000
+Make: FORD, Model: RANGER, Year: 2021, Price: $25000
+Make: TESLA, Model: MODEL3, Year: 2018, Price: $50000
+"""
+    assert bst.postOrder() == \
+"""\
+Make: FORD, Model: RANGER, Year: 2021, Price: $25000
+Make: MERCEDES, Model: SPRINTER, Year: 2022, Price: $40000
+Make: MERCEDES, Model: SPRINTER, Year: 2014, Price: $25000
+Make: TESLA, Model: MODEL3, Year: 2018, Price: $50000
+Make: NISSAN, Model: LEAF, Year: 2018, Price: $18000
+"""
 
 def test_getSuccessorNone():
     bst = CarInventory()
